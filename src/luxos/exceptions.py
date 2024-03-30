3f1f8c3627d7f3a5138c920a0768cb4461aa8757
Antonio Cavallo <a.cavallo@cavallinux.eu>
2024-03-30 08:43:18 -0400

    reorg exceptions

--- src/luxos/exceptions.py
+++ src/luxos/exceptions.py
@@ -1,10 +1,24 @@
-class LuxosBaseExceptio(Exception):
+class LuxosBaseException(Exception):
     pass
 
 
-class MinerMalformedMessageError(LuxosBaseExceptio):
+class MinerConnectionError(LuxosBaseException):
+    def __init__(self, host: str, port: int, *args, **kwargs):
+        super().__init__(host, port, *args, **kwargs)
+        self.address = (host, port)
+
+    def __str__(self):
+        return (f"<{self.address[0]}:{self.address[1]}>: {self.__class__.__name__}, "
+                f"{self.args[2] if self.args[2:] else 'unknown reason'}")
+
+
+class MinerCommandSessionAlreadyActive(MinerConnectionError):
     pass
 
 
-class MinerSessionAlreadyActive(LuxosBaseExceptio):
+class MinerCommandTimeoutError(MinerConnectionError):
+    pass
+
+
+class MinerCommandMalformedMessageError(MinerConnectionError):
     pass
