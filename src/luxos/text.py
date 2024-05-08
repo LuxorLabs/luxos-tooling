b796a4eee9dfe21b16e4ce159f5371b0338c05ad
Antonio Cavallo <a.cavallo@cavallinux.eu>
2024-05-08 07:56:10 -0400

    updates

---
+++ src/luxos/text.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+import io
+
+
+def indent(txt: str, pre: str = " " * 2) -> str:
+    """simple text indentation"""
+
+    from textwrap import dedent
+
+    txt = dedent(txt)
+    if txt.endswith("\n"):
+        last_eol = "\n"
+        txt = txt[:-1]
+    else:
+        last_eol = ""
+
+    result = pre + txt.replace("\n", "\n" + pre) + last_eol
+    return result if result.strip() else result.strip()
+
+
+def md(txt: str, md: bool | None = None, **kwargs) -> str:
+    """convert text to markdown if rich library is present"""
+    try:
+        from rich.markdown import Markdown
+
+        md = True if md is None else md
+    except ModuleNotFoundError:
+        md = False
+
+    if md:
+        from rich.console import Console
+
+        buf = io.StringIO()
+        console = Console(file=buf, **kwargs)
+        console.print(Markdown(indent(txt, "")))
+        return buf.getvalue()
+    else:
+        return indent(txt, "")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,3 +29,51 @@ def test_callme():
             assert isinstance(args, argparse.Namespace)
 
         main2()
+
+
+def test_wrapped_cli():
+    @cli.cli()
+    def main(parser):
+        """a simple main function
+
+           Lorem Ipsum is simply dummy text of the printing and
+         typesetting industry. Lorem Ipsum has been the industry's standard
+        dummy text ever since the 1500s, when an unknown printer
+          took a galley of type and scrambled it to make a type specimen book.
+        """
+        assert isinstance(parser, argparse.ArgumentParser)
+
+    @cli.cli()
+    def main2(parser):
+        """another simple main function
+
+        Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book.
+
+        """
+        assert isinstance(parser, argparse.ArgumentParser)
+
+    assert (
+        main.attrs["doc"]
+        == """a simple main function
+
+            Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+         dummy text ever since the 1500s, when an unknown printer
+           took a galley of type and scrambled it to make a type specimen book.
+        """
+    )
+
+    assert (
+        main2.attrs["doc"]
+        == """another simple main function
+
+           Lorem Ipsum is simply dummy text of the printing and
+               typesetting industry. Lorem Ipsum has been the industry's standard
+             dummy text ever since the 1500s, when an unknown printer
+           took a galley of type and scrambled it to make a type specimen book.
+
+        """
+    )
