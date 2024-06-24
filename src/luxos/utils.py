57182dc446def7e5018f08fdaaa9cf63e8a1e913
Antonio Cavallo <a.cavallo@cavallinux.eu>
2024-06-24 21:32:53 +0200

    can wrap launch result with LuxosLaunchResult

--- src/luxos/utils.py
+++ src/luxos/utils.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import asyncio
+import dataclasses as dc
 import functools
 import traceback
 from typing import Any, Callable
@@ -20,15 +21,26 @@ from luxos.ips import ip_ranges, load_ips_from_csv  # noqa: F401
 from luxos.syncops import execute_command  # noqa: F401
 
 
+@dc.dataclass
+class LuxosLaunchResult:
+    host: str
+    port: int
+    data: Any
+
+    @property
+    def address(self):
+        return f"{self.host}:{self.port}"
+
+
 async def launch(
     addresses: list[tuple[str, int]], call: Callable[[str, int], Any], *args, **kwargs
 ) -> Any:
     """launch an async function on a list of (host, port) miners
 
     Special kwargs:
-        - batch execute operation in group of batch tasks (rate limiting)
-        - naked do not wrap the call, so is up to you catching exceptions (default None)
-
+        - batch: execute operation in group of batch tasks (rate limiting)
+        - naked: do not wrap the call, so is up to you catching exceptions
+        - raw: do not wrap the result in
     Eg.
         async printme(host, port, value):
             print(await rexec(host, port, "version"))
@@ -37,12 +49,14 @@ async def launch(
 
     # a naked options, wraps the 'call' and re-raise exceptions as LuxosLaunchError
     naked = kwargs.pop("naked") if "naked" in kwargs else None
+    raw = kwargs.pop("raw") if "raw" in kwargs else True
 
     def wraps(fn):
         @functools.wraps(fn)
         async def _fn(host: str, port: int):
             try:
-                return await fn(host, port)
+                data = await fn(host, port)
+                return data if raw else LuxosLaunchResult(host, port, data)
             except asyncio.TimeoutError as exc:
                 tback = "".join(traceback.format_exc())
                 raise LuxosLaunchTimeoutError(tback, host, port) from exc
