2f6a7b6884ba8cda0c10fd8c0ad5558e9de43c7a
Antonio Cavallo <a.cavallo@cavallinux.eu>
2024-09-20 22:26:15 +0200

    updates

--- src/luxos/cli/shared.py
+++ src/luxos/cli/shared.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import inspect
 import sys
 import types
 from typing import Callable
@@ -13,6 +14,15 @@ else:
     ArgsCallback = Callable
 
 
+def check_default_constructor(klass: type):
+    signature = inspect.signature(klass.__init__)  # type: ignore[misc]
+    for name, value in signature.parameters.items():
+        if name == "self":
+            continue
+        if value.default is inspect.Signature.empty:
+            raise RuntimeError(f"the {klass}() cannot be called without arguments")
+
+
 # The luxor base parser
 class LuxosParserBase(argparse.ArgumentParser):
     def __init__(self, modules: list[types.ModuleType], *args, **kwargs):
@@ -21,12 +31,16 @@ class LuxosParserBase(argparse.ArgumentParser):
         self.callbacks: list[ArgsCallback | None] = []
 
     def add_argument(self, *args, **kwargs):
-        try:
-            if issubclass(kwargs.get("type"), ArgumentTypeBase):
-                kwargs["default"] = kwargs.get("type")(kwargs.get("default"))
-                kwargs["type"] = kwargs["default"]
-        except TypeError:
-            pass
+        typ = kwargs.get("type")
+        if isinstance(typ, type) and issubclass(typ, ArgumentTypeBase):
+            check_default_constructor(typ)
+            obj = typ()
+            obj.default = kwargs.get("default")
+            kwargs["default"] = obj
+            kwargs["type"] = obj
+        if isinstance(typ, ArgumentTypeBase):
+            kwargs["default"] = typ.new2(kwargs.get("default"))
+            kwargs["type"] = kwargs["default"]
         super().add_argument(*args, **kwargs)
 
 
@@ -34,10 +48,31 @@ class ArgumentTypeBase:
     class _NA:
         pass
 
-    def __init__(self, default=_NA):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self._default = self._NA
+
+    @property
+    def default(self):
+        return self._default
+
+    @default.setter
+    def default(self, value):
+        self._default = self._validate(value)
+        return self._default
+
+    @classmethod
+    def new(cls, obj=_NA):
+        result = cls()
+        result.default = obj
+        if result.default is not ArgumentTypeBase._NA:
+            result.default = result._validate(result.default)
+        return result
+
+    def new2(self, default):
         self.default = default
-        if default is not ArgumentTypeBase._NA:
-            self.default = self._validate(default)
+        return self
 
     def __call__(self, txt):
         self._value = None
