7a2c64a2456bf025586f6a656f754316313916c8
antonio <antonio.cavallo@luxor.tech>
2024-11-14 17:24:31 -0500

    refactor luxor-run and adds support for tqdm

--- src/luxos/scripts/luxos_run.py
+++ src/luxos/scripts/luxos_run.py
@@ -7,6 +7,7 @@ Eg.
 
     # my-script.py
     from luxos import asyncops
+
     async def main(host: str, port: int):
         res = await asyncops.rexec(host, port, "version")
         return asyncops.validate(res, "VERSION", 1, 1)
@@ -24,12 +25,15 @@ from __future__ import annotations
 
 import argparse
 import asyncio
+import functools
 import inspect
 import json
 import logging
 import pickle
-import sys
 from pathlib import Path
+from typing import Any, Callable
+
+import tqdm
 
 from luxos import misc, text, utils
 
@@ -42,16 +46,16 @@ def add_arguments(parser: cli.LuxosParserBase) -> None:
     cli.flags.add_arguments_new_miners_ips(parser)
     cli.flags.add_arguments_rexec(parser)
     parser.add_argument("script", type=Path, help="python script to run")
+    parser.add_argument("parameters", nargs="*")
+
+    parser.add_argument("-s", "--setup", help="script setup up function (non async)")
     parser.add_argument(
-        "-e",
-        "--entry-point",
-        dest="entrypoint",
-        help="script entry point",
-        default="main",
+        "-e", "--entry-point", dest="entrypoint", help="script entry point"
     )
     parser.add_argument(
-        "-t", "--teardown", dest="teardown", help="script tear down function"
+        "-t", "--teardown", help="script tear down function (non async)"
     )
+
     parser.add_argument("-n", "--batch", type=int, help="limit parallel executions")
     parser.add_argument(
         "--list", action="store_true", help="just display the machine to run script"
@@ -67,6 +71,16 @@ def process_args(args: argparse.Namespace):
         args.error("need a miners flag (eg. --range/--ipfile)")
     if not args.script.exists():
         args.error(f"missings script file {args.script}")
+    if args.script.suffix.upper() not in {".PY"}:
+        args.error(f"script must end with .py: {args.script}")
+
+
+def apply_magic_arguments(fn: Callable, magic: dict[str, Any]) -> Callable:
+    sig = inspect.signature(fn)
+    arguments = sig.bind_partial(
+        **{k: v for k, v in magic.items() if k in sig.parameters}
+    )
+    return functools.partial(fn, *arguments.args, **arguments.kwargs)
 
 
 @cli.cli(add_arguments=add_arguments, process_args=process_args)
@@ -76,34 +90,66 @@ async def main(args: argparse.Namespace):
             print(address)
         return
 
-    # prepend the script dir to pypath
-    log.debug("inserting %s in PYTHONPATH", args.script.parent)
-    sys.path.insert(0, str(args.script.parent))
-
+    # load the module
     module = misc.loadmod(args.script)
 
-    entrypoint = getattr(module, args.entrypoint, None)
-    if not entrypoint:
-        args.error(f"no entry point {args.entrypoint} in {args.script}")
-        return
+    # assign the setup/entrypoint/teardown functions
+    setup = None
+    if args.setup != "":
+        setup = getattr(module, args.setup or "setup", None)
+        if args.setup and not setup:
+            args.error(f"no setup function {args.setup} in {args.script}")
+    log.debug(
+        "%susing setup function%s",
+        "" if setup else "not ",
+        f" {setup}" if setup else "",
+    )
+
+    if not (entrypoint := getattr(module, args.entrypoint or "main", None)):
+        args.error(f"no entry point {args.entrypoint or 'main'} in {args.script}")
+    if not inspect.iscoroutinefunction(entrypoint):
+        args.error(
+            f"entry point is not an async function: {args.entrypoint} in {args.script}"
+        )
+
+    if set(inspect.signature(entrypoint).parameters) - {"host", "port"}:  # type: ignore
+        args.error(
+            f"entry point {args.entrypoint or 'main'} function "
+            "must have (host, port) signature"
+        )
+    log.debug("using entrypoint function %s", entrypoint)
 
     teardown = None
-    if args.teardown == "":
-        pass
-    elif args.teardown:
-        if not hasattr(module, args.teardown):
-            args.error(f"no tear down function {args.teardown} in {args.script}")
-        teardown = getattr(module, args.teardown, None)
-    elif hasattr(module, "teardown"):
-        teardown = getattr(module, "teardown")
+    if args.teardown != "":
+        teardown = getattr(module, args.teardown or "teardown", None)
+        if args.teardown and not teardown:
+            args.error(f"no teardown function {args.teardown} in {args.script}")
+    log.debug(
+        "%susing teardown function%s",
+        "" if teardown else "not ",
+        f" {teardown}" if teardown else "",
+    )
 
-    result = {}
+    # these are magic values passed to all setup/main/teardown functions
+    magic = {"addresses": args.addresses, "parameters": args.parameters, "result": {}}
 
-    def callback(result):
-        log.info("processed %i / %i", len(result), len(args.addresses))
+    # ok, execute here
 
+    if setup:
+        apply_magic_arguments(setup, magic)()
+
+    progress = tqdm.tqdm(total=len(args.addresses))
+
+    def callback(addresses):
+        progress.update(len(addresses))
+
+    result = magic["result"]
     for data in await utils.launch(
-        args.addresses, entrypoint, batch=args.batch, asobj=True, callback=callback
+        args.addresses,
+        entrypoint,  # type: ignore
+        batch=args.batch,
+        asobj=True,
+        callback=callback,
     ):
         if isinstance(data, utils.LuxosLaunchTimeoutError):
             log.warning(
@@ -114,7 +160,7 @@ async def main(args: argparse.Namespace):
             )
         elif isinstance(data, utils.LuxosLaunchError):
             log.warning(
-                "insternal error from %s: %s\n%s",
+                "internal error from %s: %s\n%s",
                 data.address,
                 data.brief,
                 text.indent(data.traceback or "", "| "),
@@ -123,11 +169,7 @@ async def main(args: argparse.Namespace):
             result[data.address] = data.data
 
     if teardown:
-        if "result" in inspect.signature(teardown).parameters:
-            newresult = teardown(result)
-        else:
-            newresult = teardown()
-        result = newresult or result
+        result = apply_magic_arguments(teardown, magic)() or result
 
     if args.json:
         print(json.dumps(result, indent=2))
